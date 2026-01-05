1ac286f54cd88483459d978e85f27743fdbf6727
NewPrint3D <NewPrint3D@proton.me>
2026-01-05 10:32:27 +0100

    Update resizable.tsx

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -2,50 +2,73 @@
 
 import * as React from 'react'
 import { GripVerticalIcon } from 'lucide-react'
-import { PanelGroup, Panel, PanelResizeHandle } from 'react-resizable-panels'
+import {
+  PanelGroup,
+  Panel,
+  PanelResizeHandle,
+} from 'react-resizable-panels'
 
 import { cn } from '@/lib/utils'
 
-const ResizablePanelGroup = React.forwardRef<
-  React.ElementRef<typeof PanelGroup>,
-  React.ComponentPropsWithoutRef<typeof PanelGroup>
->(({ className, ...props }, ref) => (
-  <PanelGroup
-    ref={ref}
-    className={cn(
-      'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
-      className
-    )}
-    {...props}
-  />
-))
-ResizablePanelGroup.displayName = 'ResizablePanelGroup'
+/* ================================
+   Panel Group
+================================ */
 
-const ResizablePanel = React.forwardRef<
-  React.ElementRef<typeof Panel>,
-  React.ComponentPropsWithoutRef<typeof Panel>
->((props, ref) => <Panel ref={ref} {...props} />)
-ResizablePanel.displayName = 'ResizablePanel'
+const ResizablePanelGroup = ({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof PanelGroup>) => {
+  return (
+    <PanelGroup
+      className={cn(
+        'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
+        className
+      )}
+      {...props}
+    />
+  )
+}
 
-const ResizableHandle = React.forwardRef<
-  React.ElementRef<typeof PanelResizeHandle>,
-  React.ComponentPropsWithoutRef<typeof PanelResizeHandle> & { withHandle?: boolean }
->(({ className, withHandle = false, ...props }, ref) => (
-  <PanelResizeHandle
-    ref={ref}
-    className={cn(
-      'relative flex w-px items-center justify-center bg-border focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full',
-      className
-    )}
-    {...props}
-  >
-    {withHandle && (
-      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-background">
-        <GripVerticalIcon className="h-2.5 w-2.5" />
-      </div>
-    )}
-  </PanelResizeHandle>
-))
-ResizableHandle.displayName = 'ResizableHandle'
+/* ================================
+   Panel
+================================ */
 
-export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
+const ResizablePanel = (
+  props: React.ComponentPropsWithoutRef<typeof Panel>
+) => {
+  return <Panel {...props} />
+}
+
+/* ================================
+   Resize Handle
+================================ */
+
+const ResizableHandle = ({
+  withHandle,
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof PanelResizeHandle> & {
+  withHandle?: boolean
+}) => {
+  return (
+    <PanelResizeHandle
+      className={cn(
+        'relative flex w-px items-center justify-center bg-border focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring',
+        className
+      )}
+      {...props}
+    >
+      {withHandle && (
+        <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-background">
+          <GripVerticalIcon className="h-2.5 w-2.5" />
+        </div>
+      )}
+    </PanelResizeHandle>
+  )
+}
+
+export {
+  ResizablePanelGroup,
+  ResizablePanel,
+  ResizableHandle,
+}
