ea6ee87b6b5653bdfd9de0c0fc2e3f0cc0888ede
NewPrint3D <NewPrint3D@proton.me>
2026-01-05 10:00:42 +0100

    Update resizable.tsx

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -2,9 +2,8 @@
 
 import * as React from 'react'
 import { GripVerticalIcon } from 'lucide-react'
-import * as ResizablePrimitive from "react-resizable-panels"
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels"
 import { cn } from '@/lib/utils'
-
 function ResizablePanelGroup({
   className,
   ...props
